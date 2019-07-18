945bf2dd8a9e3e6c2aad2bbdf65fe5d9c40e7578
Travis snɯǝᗡɔW <aoeu@users.noreply.github.com>
2019-07-18 18:32:56 -0400

    Add fish function for setting acme-like minimalist colors

---
+++ fish/functions/set_fish_colors.fish
@@ -0,0 +1,18 @@
+# Set everything to black, erroneous text (that we're still typing) will be bold, not red.
+set fish_color_command black
+set fish_color_param black
+set fish_color_error black --bold
+set fish_color_redirection black
+
+# Use acme's text-selection background for seleceted text.
+set fish_color_selection black --background=yellow
+set fish_color_search_match black --bold --background=yellow
+
+set fish_color_autosuggestion purple
+set fish_color_comment grey
+
+set fish_color_end green
+
+set fish_color_match blue
+set fish_color_escape blue
+set fish_color_operator blue
