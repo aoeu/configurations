48e879d1d301f02ae826a84d37d5086edbb6f588
Travis snɯǝᗡɔW <aoeu@users.noreply.github.com>
2019-07-18 20:05:11 -0400

    Add framebuffer-specific version of fish color setter function

---
+++ fish/functions/set_fish_colors_framebuffer.fish
@@ -0,0 +1,21 @@
+function set_fish_colors_framebuffer
+# Set everything to white, erroneous text (that we're still typing) will be bold, not red.
+set fish_color_command white
+set fish_color_param white
+set fish_color_error white --bold
+set fish_color_redirection white
+set fish_color_error --bold
+
+# Use acme's text-selection background for seleceted text.
+set fish_color_selection black --background=yellow
+set fish_color_search_match black --bold --background=yellow
+
+set fish_color_autosuggestion purple
+set fish_color_comment grey
+
+set fish_color_end green
+
+set fish_color_match blue
+set fish_color_escape blue
+set fish_color_operator blue
+end
