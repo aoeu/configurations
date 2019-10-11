c0230634c787622bc49e7235a05db88ff0c8c4fd
Travis snɯǝᗡɔW <aoeu@users.noreply.github.com>
2019-10-11 03:19:32 -0400

    Make up function more robust

--- fish/functions/up.fish
+++ fish/functions/up.fish
@@ -1,5 +1,13 @@
 function up
-	for i in (seq $argv)
-		cd ..
+	set usage 'usage: up [NUM_DIRECTORIES]'
+	set n $argv[1]
+	test -z "$n" ; and cd ..; and return 0
+	test "$n" -eq "$n" ^ /dev/null ; or begin;
+	       	echo "$usage"; and return 1
 	end
+	set path ""
+	for i in (seq $argv[1])
+		set path "$path../"
+	end
+	cd $path
 end
