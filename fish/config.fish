a9bdad27fc8709483c2846591dc07445d2a2b14a
Travis snɯǝᗡɔW <aoeu@users.noreply.github.com>
2019-10-14 02:43:28 -0400

    Update fish config since profile is used anyway

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,4 @@
-set -x GOROOT_BOOTSTRAP "/opt/go1.4"
-set -x GOROOT "/opt/go"
-set -x GOPATH "/opt/ir"
-set -x GOBIN "$GOPATH/bin"
-set -x PLAN9 "/opt/plan9port"
-set -x EDITOR "$GOBIN/A"
-set -x ANDROID_HOME "$HOME/Developer/android"
-
-set PATH  "$HOME/Developer/bin" "/opt/local/bin" "/opt/local/sbin" "/usr/local/bin" "/usr/bin" "/bin" "/usr/sbin" "/sbin"  "$GOROOT/bin" "$GOPATH/bin" "$PLAN9/bin" "$HOME/.cargo/bin" "$ANDROID_HOME/platform-tools" "$ANDROID_HOME/tools/bin" "$ANDROID_HOME/build-tools/25.0.2" "/opt/flutter/bin"
-
+. ~/configurations/fish/functions/set_fish_colors.fish
+# set_fish_colors
+set -x LONG_DIR "$HOME/ir/src/github.com/aoeu"
+set -x SHORT_DIR "…"
