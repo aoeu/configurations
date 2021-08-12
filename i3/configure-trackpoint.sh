64713a02d268c4727fc1ef9e8d1f6cd1272e3754
Travis snɯǝᗡɔW <aoeu@users.noreply.github.com>
2021-08-12 00:34:34 -0400

    Work around multiple devices with the same TrackPoint name (inexplicably) appearing in xinput list

--- i3/configure-trackpoint.sh
+++ i3/configure-trackpoint.sh
@@ -2,15 +2,19 @@
 set -e
 
 # Usually the track-point device is named "TPPS/2 IBM TrackPoint" or "TPPS/2 Elan TrackPoint"
-trackpointDeviceName=`xinput list | grep 'TPPS.*TrackPoint' | awk '{print $3" "$4" "$5}'`
+# trackpointDeviceName=`xinput list | grep 'TPPS.*TrackPoint' | awk '{print $3" "$4" "$5}'`
 
-# We could also use the device ID instead of the device name, but let's not and know we could.
+# Ideally, we would use the device name instead of the device ID, but the device name can appear twice(!)
+# and Xorg will request picking an ID instead. So we'll naively brute-force all matching IDs in a loop.
 # trackpointDeviceID=`xinput list | grep 'TPPS.*TrackPoint' | sed -E 's/^.*id=([0-9]+).*$/\1/g'`
+for trackpointDeviceID in `xinput list | grep 'TPPS.*TrackPoint' | sed -E 's/^.*id=([0-9]+).*$/\1/g'`
+do
+	# Emulate the middle mouse button.
+	xinput set-prop "$trackpointDeviceID" 'libinput Middle Emulation Enabled' 1
 
-# Emulate the middle mouse button.
-xinput set-prop "$trackpointDeviceName" 'libinput Middle Emulation Enabled' 1
+	# Enable clicking and dragging of middle mouse button (instead of track-pad based scrolling).
+	# https://bbs.archlinux.org/viewtopic.php?pid=1791624#p1791624
+	# https://wayland.freedesktop.org/libinput/doc/latest/scrolling.html
+	xinput set-prop "$trackpointDeviceID" 'libinput Scroll Method Enabled' 0 0 0
 
-# Enable clicking and dragging of middle mouse button (instead of track-pad based scrolling).
-# https://bbs.archlinux.org/viewtopic.php?pid=1791624#p1791624
-# https://wayland.freedesktop.org/libinput/doc/latest/scrolling.html
-xinput set-prop "$trackpointDeviceName" 'libinput Scroll Method Enabled' 0 0 0
+done
