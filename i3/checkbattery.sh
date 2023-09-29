f8e6620659683b39219473aee7320a321897b5f5
Travis snɯǝᗡɔW <aoeu@users.noreply.github.com>
2023-09-29 16:37:55 -0400

    Add script to warn about low battery via a cron job

---
+++ i3/checkbattery.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Utilize with `crontab -e` (no sudo) and `*/15 * * * * /fully/qualified/filepath/checkbattery.sh`.
+threshold="10" message="Battery power is lower than $threshold%"
+battery_percent=$(acpi --battery | sed --regexp-extended 's/.*\s([0-9]+)%.*/\1/')
+
+test "$battery_percent" -lt "$threshold" && \
+	DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id --user)/bus" \
+	notify-send --wait --urgency=critical "$message"
