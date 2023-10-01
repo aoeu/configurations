06a05406ab1863619d1d268da83a81bc97343da6
Travis snɯǝᗡɔW <aoeu@users.noreply.github.com>
2023-09-30 22:28:56 -0400

    Read battery percentage directly

--- i3/checkbattery.sh
+++ i3/checkbattery.sh
@@ -5,7 +5,7 @@ test "Charging" = $(cat /sys/class/power_supply/BAT0/status) && \
 	exit 0
 
 threshold="10" message="Battery power is lower than $threshold%"
-battery_percent=$(acpi --battery | sed --regexp-extended 's/.*\s([0-9]+)%.*/\1/')
+battery_percent=$(head /sys/class/power_supply/BAT0/capacity)
 
 test "$battery_percent" -lt "$threshold" && \
 	DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id --user)/bus" \
