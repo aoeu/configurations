1bc0f961495ef965f5382d1e2f2d7d3a5127e5dc
Travis snɯǝᗡɔW <aoeu@users.noreply.github.com>
2023-09-30 22:28:10 -0400

    Exit battery checker if charging

--- i3/checkbattery.sh
+++ i3/checkbattery.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 # Utilize with `crontab -e` (no sudo) and `*/15 * * * * /fully/qualified/filepath/checkbattery.sh`.
+
+test "Charging" = $(cat /sys/class/power_supply/BAT0/status) && \
+	exit 0
+
 threshold="10" message="Battery power is lower than $threshold%"
 battery_percent=$(acpi --battery | sed --regexp-extended 's/.*\s([0-9]+)%.*/\1/')
 
