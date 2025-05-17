a14ab70194b04d695fd1af8f31e8de0fdea342d8
Travis snɯǝᗡɔW <aoeu@users.noreply.github.com>
2025-05-16 22:14:53 -0400

    Remove former use of fish shell's stderr operator

--- fish/functions/up.fish
+++ fish/functions/up.fish
@@ -2,7 +2,7 @@ function up
 	set usage 'usage: up [NUM_DIRECTORIES]'
 	set n $argv[1]
 	test -z "$n" ; and cd ..; and return 0
-	test "$n" -eq "$n" ^ /dev/null ; or begin;
+	test "$n" -eq "$n" 2> /dev/null ; or begin;
 	       	echo "$usage"; and return 1
 	end
 	set path ""
