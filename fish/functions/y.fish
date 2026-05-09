edb93254c53ea1e55267794e471ba8e1dc33d103
Travis snɯǝᗡɔW <aoeu@users.noreply.github.com>
2026-05-09 13:07:46 -0400

    Add wrapper for yazi to follow directory changes

---
+++ fish/functions/y.fish
@@ -0,0 +1,10 @@
+function y
+    set prevWD "$PWD"
+    pushd .
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
